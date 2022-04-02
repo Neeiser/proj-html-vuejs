f73f7fe554d004f844d94e9a14b44f7664af2b53
Neeiser <stortini.study@gmail.com>
2022-04-02 17:35:12 +0200

    color variables set

--- src/components/header-components/JumboHeaderKeynote.vue
+++ src/components/header-components/JumboHeaderKeynote.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="jumbo-container">
-
+    <div class="jumbo-container text-vertical-centering">
+        <h2>Conference on</h2>
+        <h1>Social Networks</h1>
+        <hr>
+        <span>23 - 26 may 2019 / sans francisco / 8 speakers</span>
     </div>
 </template>
 
@@ -12,10 +15,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/styles/partials/generic.scss';
+
 .jumbo-container{
     height: 600px;
     background-image: url('../../assets/img/slider-1-bg.jpg');
-    background-size: fill;
     background-position: center;
+    text-transform: uppercase;
+    hr{
+        width: 500px;
+        border: 2px solid white;
+    }
 }
 </style>
